08b3845b58f8b58213f4481f85bd838cd12e6720
ai0by <root@biyeai.com>
2017-06-21 22:13:23 +0800

    更新数据库

--- aVane/models.py
+++ aVane/models.py
@@ -1,5 +1,31 @@
 # coding=utf-8
 from aVane import db
+from datetime import datetime
+
+# 定义评论
+class Comment(db.model):
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    content = db.Column(db.String(2048))
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    image_id = db.Column(db.Integer,db.ForeignKey('image.id'))
+    status = db.Column(db.Integer,default=0)# 判断该评论是否被删除,不在数据库中物理删除,默认0不删除
+
+
+# 定义图片
+class Image(db.model):
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    url = db.Column(db.String(512))
+    create_date = db.Column(db.DateTime)
+    comment = db.relationship('Comment')
+
+    def __init__(self,Url,User_id):
+        self.url = Url
+        self.user_id = User_id
+        self.create_date = datetime.now()
+
+    def __repr__(self):
+        return '<Image %d %d %s %s>' % (self.id,self.user_id,self.url,self.create_date)
 
 # 定义用户
 class User(db.Model):
@@ -8,6 +34,8 @@ class User(db.Model):
     username = db.Column(db.String(80),unique=True)
     password = db.Column(db.String(32))
     head_url = db.Column(db.String(256))
+    images = db.relationship('Image')
+    comment = db.relationship('Comment')
     # 定义这个类的构造函数,分别赋初始值
     def __init__(self,Username,Password):
         self.username = Username
--- manage.py
+++ manage.py
@@ -2,19 +2,23 @@
 
 from aVane import app,db
 from flask_script import Manager
-from aVane.models import User
+from aVane.models import User,Image
 
 manager = Manager(app)
 
+def get_image_url():
+    return '/static/default.jpg'
 @manager.command
 def init_database():
-    # 删除所有数据库内容
+    # 删除所有数据库内容,测试用
     db.drop_all()
-    # 根据数据库内的定义创建数据
+    # 根据数据库内的定义创建数据,测试用
     db.create_all()
     # 生成100个随机用户
     for i in range(0,100):
         db.session.add(User('User'+str(i),'a'+str(i)))
+        for j in range(0,3):
+            db.session.add(Image(get_image_url(),i+1))
     # 提交操作
     db.session.commit()
 
